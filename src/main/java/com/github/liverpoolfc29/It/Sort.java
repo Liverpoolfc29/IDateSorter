87e9235c1f2a38497aec8a132a0ace450d4fb0d4
Miha <Liverpoolfc291892@gmail.com>
2023-02-28 16:40:04 +0300

    second step

--- src/main/java/com/github/liverpoolfc29/It/Sort.java
+++ src/main/java/com/github/liverpoolfc29/It/Sort.java
@@ -3,14 +3,53 @@ package com.github.liverpoolfc29.It;
 import com.github.liverpoolfc29.It.Data.IDateSorter;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
+import java.time.Month;
+import java.util.*;
 
 public class Sort implements IDateSorter {
 
     @Override
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
-        return null;
+        List<LocalDate> localDatesWithLetterR = sortMonthWithLetterR(unsortedDates);
+        List<LocalDate> localDateListWithoutLetterR = sortMonthWithoutLetterR(unsortedDates);
+        sortByMonth(localDatesWithLetterR);
+
+        return localDatesWithLetterR;
+    }
+
+    private List<LocalDate> sortMonthWithLetterR(List<LocalDate> unsortList) {
+        List<LocalDate> tempListWithLetterR = new ArrayList<>();
+        for (LocalDate localDate : unsortList) {
+            String monthName = localDate.getMonth().toString().toLowerCase();
+            if (monthName.contains("r")) {
+                tempListWithLetterR.add(localDate);
+            }
+        }
+        return tempListWithLetterR;
+    }
+
+    private List<LocalDate> sortMonthWithoutLetterR(List<LocalDate> unsortList) {
+        List<LocalDate> tempListWithoutLetterR = new ArrayList<>();
+        for (LocalDate localDate : unsortList) {
+            String monthName = localDate.getMonth().toString().toLowerCase();
+            if (!monthName.contains("r")) {
+                tempListWithoutLetterR.add(localDate);
+            }
+        }
+        return tempListWithoutLetterR;
+    }
+
+    private void sortByMonth(List<LocalDate> list) {
+        LocalDate templocalDate;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 1; j < (list.size() - i); j++) {
+                if (list.get(j - 1).getMonth().getValue() > list.get(j).getMonth().getValue()) {
+                    templocalDate = list.get(j - 1);
+                    list.set(j - 1, list.get(j));
+                    list.set(j, templocalDate);
+                }
+            }
+        }
     }
 
 }
--- src/main/java/com/github/liverpoolfc29/MainClass.java
+++ src/main/java/com/github/liverpoolfc29/MainClass.java
@@ -1,8 +1,34 @@
 package com.github.liverpoolfc29;
 
+import com.github.liverpoolfc29.It.Sort;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class MainClass {
 
     public static void main(String[] args) {
-        
+        LocalDate january = LocalDate.of(2023, 1, 1);
+        LocalDate february = LocalDate.of(2023, 2, 1);
+        LocalDate march = LocalDate.of(2023, 3, 1);
+        LocalDate april = LocalDate.of(2023, 4, 1);
+        LocalDate may = LocalDate.of(2023, 5, 1);
+        LocalDate june = LocalDate.of(2023, 6, 1);
+        LocalDate july = LocalDate.of(2023, 7, 1);
+        LocalDate august = LocalDate.of(2023, 8, 1);
+        LocalDate september = LocalDate.of(2023, 9, 1);
+        LocalDate october = LocalDate.of(2023, 10, 1);
+        LocalDate november = LocalDate.of(2023, 11, 1);
+        LocalDate december = LocalDate.of(2023, 12, 1);
+
+        List<LocalDate> localDateList = new ArrayList<>(Arrays.asList(february, march, january, april, may, june, july, august, september, october, november, december));
+
+        Sort sort = new Sort();
+        Collection<LocalDate> localDates = sort.sortDates(localDateList);
+        System.out.println(localDates);
     }
+
 }
